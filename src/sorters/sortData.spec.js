bd57ef5886c7ac6233358c11c8ba86a2b63a1e1e
Westin Schepper <WestinSchepper@users.noreply.github.com>
2020-07-01 20:26:37 -0400

    Update sortData specs to work with new indexes argument

--- src/sorters/sortData.spec.js
+++ src/sorters/sortData.spec.js
@@ -7,18 +7,25 @@ jest.mock('./endsWithSort', () => jest.fn())
 
 describe('sortData', () => {
   it('calls the startsWithSort method if the type is `start`', () => {
-    sortData(['a', 'b', 'c'], 'start')
+    sortData(['a', 'b', 'c'], [], 'start')
     expect(mockStartsWithSort).toBeCalled()
   })
 
   it('calls the endsWithSort method if the type is `end`', () => {
-    sortData(['a', 'b', 'c'], 'end')
+    sortData(['a', 'b', 'c'], [], 'end')
     expect(mockEndsWithSort).toBeCalled()
   })
 
   it('returns the provided data as-is if the type is not `start` or `end`', () => {
     const input = ['a', 'b', 'c']
-    const result = sortData(input, 'any')
+    const result = sortData(input, [], 'any')
+
+    expect(result).toEqual(input)
+  })
+
+  it('returns the provided data as-is if the indexes array is not empty', () => {
+    const input = ['a', 'b', 'c']
+    const result = sortData(input, ['name'], 'any')
 
     expect(result).toEqual(input)
   })
