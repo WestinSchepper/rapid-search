8033ffb57ec7c08d26ce6ad804549c5ec4ee4968
Westin Schepper <WestinSchepper@users.noreply.github.com>
2020-07-01 21:06:59 -0400

    Add indexes to the data manager

--- src/dataManager.js
+++ src/dataManager.js
@@ -5,9 +5,9 @@ const createIndexMap = require('./factories/createIndexMap')
 const resolveDataSource = require('./helpers/resolveDataSource')
 const createSearchEngine = require('./searchers/createSearchEngine')
 
-const dataManager = (data, limit, type) => {
-  const source = sortData(data, type)
-  const indexMap = createIndexMap(source, type)
+const dataManager = (data, indexes, limit, type) => {
+  const source = sortData(data, indexes, type)
+  const indexMap = createIndexMap(source, indexes, type)
   let lastQuery = ''
   let result = []
   let searchEngine = undefined
@@ -22,14 +22,13 @@ const dataManager = (data, limit, type) => {
 
   const search = (query) => {
     const dataSource = resolveDataSource(source, result, indexMap, query, lastQuery, type)
-    searchEngine = createSearchEngine(dataSource, query, limit, type)
+    searchEngine = createSearchEngine(dataSource, indexes, query, limit, type)
     result = searchEngine.next().value
     lastQuery = query
 
     return getResult()
   }
 
-  // TODO: Implement `canLoadMore` method
   const loadMore = () => {
     const nextResult = searchEngine.next()
     if (!isUndefined(searchEngine) && !nextResult.done) {
